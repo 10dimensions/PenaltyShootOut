7c9b3ec350973779a556133045f3173dcc84826a
_vishal <>
2019-05-23 14:46:11 +0530

    init

---
+++ Assets/Scripts/CustomJoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class CustomJoint : MonoBehaviour
+{   
+    public GameObject BodySrcManager;
+    public JointType TrackedJoint;
+    private BodySourceManager bodyManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
